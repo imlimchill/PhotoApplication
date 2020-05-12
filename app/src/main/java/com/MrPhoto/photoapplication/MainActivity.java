c2936ab2d422d9f768a016873ba12bac251a6b8c
imlimchill <cojwd300@gmail.com>
2020-05-12 22:01:15 +0900

    #1 메인화면 구성 및 아이콘 설정
    메인화면의 아이콘 위치를 버튼으로 구현한다.
    메인화면의 타이틀 창을 삭제한다.
    아이콘의 변경을 한다.

--- app/src/main/java/com/MrPhoto/photoapplication/MainActivity.java
+++ app/src/main/java/com/MrPhoto/photoapplication/MainActivity.java
@@ -1,14 +1,35 @@
 package com.MrPhoto.photoapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    /** 설정 창 버튼 */
+    Button settingBtn;
+    /** 화면 비율 변환 버튼 */
+    Button rationBtn;
+    /** 정방향, 후방향 화면 전환 버튼 */
+    Button reverseBtn;
+    /** 스티커 버튼 */
+    Button stikerBtn;
+    /** 촬영 버튼 */
+    Button photoBtn;
+    /** 필터 버튼 */
+    Button filterBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        settingBtn = (Button)findViewById(R.id.settingBtn);
+        rationBtn = (Button)findViewById(R.id.ratioBtn);
+        reverseBtn = (Button)findViewById(R.id.reverseBtn);
+        stikerBtn = (Button)findViewById(R.id.stikerBtn);
+        photoBtn = (Button)findViewById(R.id.photoBtn);
+        filterBtn = (Button)findViewById(R.id.filterBtn);
+
     }
 }
