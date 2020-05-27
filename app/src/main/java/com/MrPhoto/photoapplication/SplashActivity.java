d3f8b037d7ff9b102afbd8251dc8ce76e414a2cd
imlimchill <cojwd300@gmail.com>
2020-05-27 23:16:06 +0900

    splash기능 추가
    1. splash 화면인 activity_splash.xml 추가
    2. splash 기능을 구현한 SplashActivity.java 추가
    3. splash 화면을 Main 화면보다 먼저 출력하기 위해 Manifasts 파일의 수정

---
+++ app/src/main/java/com/MrPhoto/photoapplication/SplashActivity.java
@@ -0,0 +1,51 @@
+package com.MrPhoto.photoapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    /** 시간초를 선언해 자동으로 넘어갈 수 있도록 해주는 클래스 */
+    Handler handler = new Handler();
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            // handler로 설정해둔 시간이 끝났을 때 MainActivity로 넘어갈 수 있게해준다.
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            // 그리고 이 화면은 필요가 없기때문에 지워준다.
+            finish();
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+
+    /**
+     * 애플리케이션이 최소화되었을 때 실행하는 함수
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // handler가 예약해둔 작업을 취소한다.
+        handler.removeCallbacks(runnable);
+    }
+
+    /**
+     * 화면이 최대화가 됐을 때 실행하는 함수
+     * 1. 애플리케이션이 켜졌을 때 실행한다.
+     * 2. 애플리케이션이 최소화 되었다가 최대화 되면 실행한다.
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // handler가 하려던 작업을 다시 시작한다.
+        handler.postDelayed(runnable, 800);
+    }
+}
