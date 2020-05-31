1548f639fea2135f75f164e04ffcf6250c8189eb
DLDLMBDLDF <vldzm2964@naver.com>
2020-05-31 23:38:15 +0900

    애플리케이션 종료하기.
    1. 메인 화면에서 뒤로가기 버튼을 한 번 누르면 종료 메시지가 뜬다.
    2. 메시지가 떠있는 상태에서 한 번 더 누르면 앱이 꺼진다.

--- app/src/main/java/com/MrPhoto/photoapplication/MainActivity.java
+++ app/src/main/java/com/MrPhoto/photoapplication/MainActivity.java
@@ -60,6 +60,9 @@ public class MainActivity extends AppCompatActivity {
 
     private TextureView textureView;
 
+    //백버튼이 눌린 마지막 시간을 저장함.
+    private long backKeyPressedTime = 0;
+
     private static final SparseIntArray ORIENTATIONS = new SparseIntArray();
     static {
         ORIENTATIONS.append(Surface.ROTATION_0, 90);
@@ -134,6 +137,23 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    /**현재 시간이 마지막으로 백버튼을 누른 시간에서 2초 지났으면 마지막으로 백버튼이 눌린 시간으로 현재 시간을 생긴한다.*/
+    public void onBackPressed(){
+        if (System.currentTimeMillis() > backKeyPressedTime + 2000) {
+            backKeyPressedTime = System.currentTimeMillis();
+            Toast.makeText(this, "\'뒤로\' 버튼을 한번 더 누르면 종료됩니다.", Toast.LENGTH_SHORT).show();
+            return;//백버튼이 한 번 눌리면 종료 안내 메시지가 뜬다.
+        }
+
+        /** 마지막으로 백버튼을 눌렀던 시간에 2초를 더해 현재시간과 비교 후,
+         마지막으로 백버튼을 눌렀던 시간이 2초가 지나지 않았으면 앱을 종료시킴.
+         (메시지가 유지되는 2초동안 백버튼을 한 번 더 누르면 앱 종료)*/
+        if (System.currentTimeMillis() <= backKeyPressedTime + 2000){
+            finish();
+
+        }
     }
 
     /**
