02d5a54129e4bf7fcc3a2f0f575f091b84b066a9
DLDLMBDLDF <vldzm2964@naver.com>
2020-06-10 04:36:28 +0900

    메인화면 설정 버튼 클릭시 1.설정버튼 클릭 2.세부기능들을 사용할 수 있는 설정창이 뜸 3. popActivity 사용

--- app/src/main/java/com/MrPhoto/photoapplication/PopActivity.java
+++ app/src/main/java/com/MrPhoto/photoapplication/PopActivity.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.os.Bundle;
 import android.view.Display;
 import android.view.WindowManager;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class PopActivity extends AppCompatActivity {
@@ -14,13 +13,9 @@ public class PopActivity extends AppCompatActivity {
         setContentView(R.layout.activity_pop);
 
         Display display = ((WindowManager) getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
-
         int width = (int) (display.getWidth() * 0.9); //Display 사이즈의 90% 각자 원하는 사이즈로 설정하여 사용
-
         int height = (int) (display.getHeight() * 0.1);  //Display 사이즈의 90% 각자 원하는 사이즈로 설정하여 사용
-
         getWindow().getAttributes().width = width;
-
         getWindow().getAttributes().height = height;
     }
 }
